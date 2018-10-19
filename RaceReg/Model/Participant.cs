70e3d23440e44d9bf6cd9d7e246e1425b12f2d98
Jackson Porter <jackson_porter@outlook.com>
2018-10-19 16:58:10 -0600

    Added some changes and starting to use MVVM pattern.

--- RaceReg/Model/Participant.cs
+++ RaceReg/Model/Participant.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RaceReg.Model
 {
-    public class Participant
+    public class Participant : INotifyPropertyChanged
     {
         private int _id;
         private string _firstName;
@@ -15,11 +16,117 @@ namespace RaceReg.Model
         private string _gender;
         private DateTime _birthDate;
 
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public Affiliation Affiliation { get; set; }
-        public string Gender { get; set; }
-        public DateTime BirthDate { get; set; }
+        public int Id {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if(_id == value)
+                {
+                    return;
+                }
+
+                _id = value;
+                RaisePropertyChanged("Id");
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                if (string.Equals(_firstName, value))
+                {
+                    return;
+                }
+
+                _firstName = value;
+                RaisePropertyChanged("FirstName");
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                if (string.Equals(_lastName, value))
+                {
+                    return;
+                }
+
+                _lastName = value;
+                RaisePropertyChanged("LastName");
+            }
+        }
+        public Affiliation Affiliation
+        {
+            get
+            {
+                return _affiliation;
+            }
+            set
+            {
+                if (_affiliation == value)
+                {
+                    return;
+                }
+
+                _affiliation = value;
+                RaisePropertyChanged("Affiliation");
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                return _gender;
+            }
+            set
+            {
+                if (string.Equals(_gender, value))
+                {
+                    return;
+                }
+
+                _gender = value;
+                RaisePropertyChanged("Gender");
+            }
+        }
+        public DateTime BirthDate
+        {
+            get
+            {
+                return _birthDate;
+            }
+            set
+            {
+                if (_birthDate == value)
+                {
+                    return;
+                }
+
+                _birthDate = value;
+                RaisePropertyChanged("BirthDate");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void RaisePropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
--- RaceReg/Model/UpdateParticipant.cs
+++ RaceReg/Model/UpdateParticipant.cs
@@ -11,39 +11,49 @@ namespace RaceReg.Model
     {
         public async Task<IEnumerable<Participant>> Refresh()
         {
-            //Create a database connection
-            MySqlConnection connection = null;
-            connection = new MySqlConnection(Constants.CONNECTION_STRING);
-            connection.Open();
-
             List<Affiliation> affiliations = new List<Affiliation>();
-
+            List<Participant> participants = new List<Participant>();
             string getAffiliationsQuery = "SELECT * FROM " + Constants.AFFILIATION + " WHERE active = 1;";
-            var cmd = new MySqlCommand(getAffiliationsQuery, connection);
-            var reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                Affiliation temp = new Affiliation();
-                temp.Id = reader.GetInt32(0);
-                temp.Name = reader.GetString(1);
-                temp.Abbreviation = reader.GetString(2);
-                affiliations.Add(temp);
-            }
-
             string getParticipantsQuery = "SELECT * FROM " + Constants.PARTICIPANT + " WHERE active = 1;";
-            var cmd = new MySqlCommand(getParticipantsQuery, connection);
-            var reader = cmd.ExecuteReader();
-            while(reader.Read())
+
+            using (var connection = new MySqlConnection(Constants.CONNECTION_STRING))
             {
-                Participant temp = new Participant();
-                temp.Id = reader.GetInt32(0);
-                temp.FirstName = reader.GetString(1);
-                temp.LastName = reader.GetString(2);
-                int affId = reader.GetInt32(3);
+                await connection.OpenAsync();
+                var cmd = new MySqlCommand(getAffiliationsQuery, connection);
+                var reader = await cmd.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    Affiliation temp = new Affiliation();
+                    temp.Id = reader.GetInt32(0);
+                    temp.Name = reader.GetString(1);
+                    temp.Abbreviation = reader.GetString(2);
+                    affiliations.Add(temp);
+                }
 
-                temp.Gender = reader.GetString(4);
-
+                cmd = new MySqlCommand(getParticipantsQuery, connection);
+                reader = await cmd.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    Participant temp = new Participant();
+                    temp.Id = reader.GetInt32(0);
+                    temp.FirstName = reader.GetString(1);
+                    temp.LastName = reader.GetString(2);
+                    int affId = reader.GetInt32(3);
+                    foreach (Affiliation tempAffiliation in affiliations)
+                    {
+                        if (tempAffiliation.Id == affId)
+                        {
+                            temp.Affiliation = tempAffiliation;
+                            break;
+                        }
+                    }
+                    temp.Gender = reader.GetString(4);
+                    temp.BirthDate = reader.GetDateTime(5);
+                    participants.Add(temp);
+                }
             }
+
+            return participants;
         }
 
         public async Task<string> Save(Participant updatedParticipant)
