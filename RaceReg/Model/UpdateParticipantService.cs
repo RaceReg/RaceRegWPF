2a4b1de015502f1782f887114c5ede1a34ec2c4c
Jackson Porter <jackson_porter@outlook.com>
2018-10-19 23:13:35 -0600

    Adapting code to be MVVM

--- RaceReg/Model/UpdateParticipantService.cs
+++ RaceReg/Model/UpdateParticipantService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RaceReg.Model
 {
-    interface UpdateParticipantService
+    public interface UpdateParticipantService
     {
         Task<IEnumerable<Participant>> Refresh();
         Task<string> Save(Participant updatedParticipant);
--- RaceReg/ViewModel/MainViewModel.cs
+++ RaceReg/ViewModel/MainViewModel.cs
@@ -1,4 +1,9 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using RaceReg.Helpers;
+using RaceReg.Model;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace RaceReg.ViewModel
 {
@@ -16,10 +21,65 @@ namespace RaceReg.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        private UpdateParticipantService _updateParticipantService;
+        private IDialogService _dialogService;
+
+        public ObservableCollection<Participant> Participants
+        {
+            get;
+            private set;
+        }
+
+        private Participant _selectedParticipant;
+        public Participant SelectedParticipant
+        {
+            get
+            {
+                return _selectedParticipant;
+            }
+            set
+            {
+                if (_selectedParticipant == value)
+                {
+                    return;
+                }
+
+                _selectedParticipant = value;
+                RaisePropertyChanged("SelectedParticipant");
+            }
+        }
+
+        private RelayCommand _refreshParticipantCommand;
+        public RelayCommand RefreshParticipantCommand
+        {
+            get
+            {
+                return _refreshParticipantCommand ?? (_refreshParticipantCommand = new RelayCommand(
+                    async () =>
+                    {
+                        await Refresh();
+                    }
+                    ));
+            }
+        }
+
+        private async Task Refresh()
+        {
+            Participants.Clear();
+
+            var participants = await _updateParticipantService.Refresh();
+
+            foreach(var participant in participants)
+            {
+                Participants.Add(participant);
+            }
+        }
+
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
-        public MainViewModel()
+        public MainViewModel(UpdateParticipantService updateParticipantService, 
+            IDialogService dialogService)
         {
             ////if (IsInDesignMode)
             ////{
@@ -29,6 +89,12 @@ namespace RaceReg.ViewModel
             ////{
             ////    // Code runs "for real"
             ////}
+
+            _updateParticipantService = updateParticipantService;
+            _dialogService = dialogService;
         }
+
+        //Default constructor
+        public MainViewModel() : this(new UpdateParticipant(), new DialogService()) { }
     }
 }
