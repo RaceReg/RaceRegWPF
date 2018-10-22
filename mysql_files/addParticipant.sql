b48eb594f4933aeafce3c76f948df524b781fd40
Jackson Porter <jackson_porter@outlook.com>
2018-10-22 17:24:57 -0600

    Added add participant sql file as well as changed datagrid to show all columns.

---
+++ mysql_files/addParticipant.sql
@@ -0,0 +1,11 @@
+-- Creates a new participant
+
+INSERT INTO Participant(firstname, lastname, affiliationid, gender, birthdate, active) VALUES
+(
+	'Alex',
+	'Thayn',
+	1,
+	'm',
+	'2018-10-22',
+	1
+)
